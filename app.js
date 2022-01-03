6381398065db3d13c2f340e59fa61659ccc9553b
Jhon Arcila Castaño <jdeiber@uno27.co>
2022-01-03 13:36:58 -0500

    methods in vue

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            message: 'Hello Vue.js!',
-            courseGoal: '',
+            courseGoalA: 'Master Vue.js with amazing course',
+            courseGoalB: 'Finish the course and learn vuejs',
+            vueLink: 'https://vuejs.org/',
+        }
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.floor(Math.random() * 10) + 1;
+            if (randomNumber % 2 === 0) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
         }
     }
 });
