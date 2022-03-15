2902ebca9c80b750d8807ce30f8e116e00761bb5
Jhon Deiber Arcila Castaño <1053860350@UNO27.COM>
2022-03-15 16:02:40 -0500

    components keep-active teleport and error-alert

---
+++ section 9/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>Manage Goals</h2>
+    <button @click="setGoal" ref="goal">Set Goal</button>
+    <input type="text" />
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is invalid</h2>
+        <p>Please enter a valid goal</p>
+        <button @click="confirmError">Okay</button>
+      </error-alert>
+    </teleport>
+  </div>
+</template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
