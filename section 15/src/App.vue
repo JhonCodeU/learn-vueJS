1e81c8d7f9928b5ae8aede4f67c4f330737095fa
Jhon Deiber Arcila Castaño <1053860350@UNO27.COM>
2022-03-16 17:55:27 -0500

    Section 15 vuex

---
+++ section 15/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <base-container title="Vuex">
+    <h3>{{ counter }}</h3>
+    <the-counter></the-counter>
+    <button @click="addOne">Add 1</button>
+    <change-counter></change-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+  methods: {
+    addOne() {
+      this.$store.state.counter++;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ section 15/src/components/TheCounter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>The counter</h2>
+    <h3>{{ counter }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+};
+</script>
